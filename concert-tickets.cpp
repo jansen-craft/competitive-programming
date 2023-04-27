d26b4d63990403c53b468f6f754a9205b625a86a
Jansen Craft <89416543+jc103820@users.noreply.github.com>
2023-04-27 15:39:58 -0400

    concert tickets done

--- concert-tickets.cpp
+++ concert-tickets.cpp
@@ -1,36 +1,31 @@
 #include <iostream>
-#include <vector>
+#include <set>
 //#include <bits/stdc++.h>
 
 using namespace std;
 
 int main(){
-    int num_tickets, num_customers;
+    int num_tickets, num_customers, tmp;
     cin >> num_tickets >> num_customers;
 
-    vector<int> ticket_price (num_tickets);
-    vector<int> max_price (num_customers);
+    multiset<int> m;
 
     for (int i = 0; i < num_tickets; i++){
-        cin >> ticket_price[i];
+        cin >> tmp;
+        m.insert(tmp);
     }
 
     for (int i = 0; i < num_customers; i++){
-        cin >> max_price[i];
-    }
-
-    sort(ticket_price.begin(), ticket_price.end());
-    
-    for (int c = 0; c < num_customers; c++){
-        auto l = upper_bound(ticket_price.begin(), ticket_price.end(), max_price[c]);
-        
-        if(l == ticket_price.begin()){
-            cout << -1 << endl;
+        cin >> tmp;
+        auto p = m.upper_bound(tmp);
+        if(p == m.begin()){
+            cout << "-1 ";
         } else {
-            l--;
-            cout << *l << endl;
-            ticket_price.erase(l);
+            p--;
+            cout << *p << ' ';
+            m.erase(p);
         }
+
     }
     
     return 0;
